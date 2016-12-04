6aafca326829f5d19b17f3f39e1fc26c1d5746df
josephernest <nouvellecollection@gmail.com>
2016-12-04 20:03:07 +0100

    Hide when rendering

--- 0doc.js
+++ 0doc.js
@@ -6,6 +6,7 @@
 */
 
 //Insert includes
+$('#content').addClass('rendering');
 $('#content file').each(function(index) {
     var filename = $(this).attr('src');
     var element = this;
@@ -30,7 +31,8 @@ go = function() {
 // Render Markdown
 marked.Lexer.rules.gfm.heading = marked.Lexer.rules.normal.heading;  // fix to allow ##title instead of ## title (see https://github.com/chjj/marked/issues/642#issuecomment-130213790)
 marked.Lexer.rules.tables.heading = marked.Lexer.rules.normal.heading;
-$('#content').html(marked($('#content').text()));
+$('#content').html(marked($('#content').text())).removeClass('rendering');
+
 
 // TOC building, inspired of http://stackoverflow.com/a/40946392/1422096
 var toc = document.getElementById('toc_ul'); 
